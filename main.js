27f5bb85dc9dff9c47f0dd6cb8d413a52f7b629f
Josh Batley <jbatley12@gmail.com>
2018-08-18 18:52:42 +0100

    Icons and start of script

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
+const url = window.location.host;
 
+const blockedContent = '.svgIcon--star';
+
+window.addEventListener('load', function () {
+    chrome.tabs.getSelected(null,function(tab){
+        myURL=tab.url;
+    });
+// window.alert(url)
+const isMedium = url.includes('medium');
+
+let parents = [].slice.call(document.querySelectorAll(blockedContent)).map(i => i.closest('.streamItem'));
+
+if (isMedium) {
+  chrome.browserAction.setIcon({path: './icons/icon_16.png'});
+}
+
+parents.forEach(e => {
+  console.log(e);
+  while(e.firstElementChild) {
+    e.removeChild(e.firstElementChild)
+  }
+})
